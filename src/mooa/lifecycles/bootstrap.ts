bb01f02cdeb6c9d51ccee5516be137e7d9c86978
Phodal HUANG <h@phodal.com>
2018-03-08 16:59:43 +0800

    [mooa] thinking in bs

--- src/mooa/lifecycles/bootstrap.ts
+++ src/mooa/lifecycles/bootstrap.ts
@@ -1,3 +1,3 @@
 export async function toBootstrapPromise(app) {
-
+  // loadAllAssets
 }
--- src/mooa/mooa.ts
+++ src/mooa/mooa.ts
@@ -1,6 +1,7 @@
 import {StatusEnum} from './constants';
 
 class Mooa {
+  started = false;
   apps = [];
 
   registerApplication(appName: string, appConfig?, activeWhen?: {}, customProps: object = {}) {
@@ -12,17 +13,54 @@ class Mooa {
       throw new Error(`The activeWhen argument must be a function`);
     }
 
-    this.apps.push({
+    const appConfig = {
       name: appName,
       appConfig,
       activeWhen,
       status: StatusEnum.NOT_LOADED,
       customProps: customProps
-    });
+    };
+
+    this.apps.push(this.createApp(appConfig));
+
+    this.reRouter();
   }
 
   start() {
-    console.log(this.apps);
+    this.started = true;
+    return this.reRouter();
+  }
+
+  private reRouter() {
+    async function loadApps() {
+
+    }
+
+    async function performAppChanges() {
+      // unload -> unmount
+      // load -> mount
+      const appsToLoad = this.apps;
+      const loadThenMountPromises = appsToLoad.map(app => {
+        // return toLoadPromise(app)
+        //   .then(toBootstrapPromise)
+        //   .then(async function(app) {
+        //     await unmountAllPromise;
+        //     return toMountPromise(app);
+        //   });
+      });
+
+    }
+
+    if (this.started) {
+      return performAppChanges();
+    } else {
+      return loadApps();
+    }
+  }
+
+  private createApp(appConfig: object) {
+    const app = appConfig.map(x => Object.assign({}, x));
+    return app;
   }
 }
 
