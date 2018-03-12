286367ba7af796ee4836c02faabddfd631b46762
Phodal HUANG <h@phodal.com>
2018-03-12 21:50:46 +0800

    [T] remove empty line

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
-
 
 @NgModule({
   declarations: [
@@ -19,4 +17,5 @@ import {AppRoutingModule} from './app-routing.module';
   providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
