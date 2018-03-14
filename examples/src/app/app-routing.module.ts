772b7798a0e7c003add2b8118396ee0ebec6f532
Phodal HUANG <h@phodal.com>
2018-03-14 14:04:14 +0800

    [examples] add basic router

--- examples/src/app/app-routing.module.ts
+++ examples/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {AppComponent} from './app.component';
+import {ReaderComponent} from './reader/reader.component';
+import {GamesComponent} from './games/games.component';
+import {ShoppingComponent} from './shopping/shopping.component';
 
 @NgModule({
   imports: [
@@ -8,6 +11,18 @@ import {AppComponent} from './app.component';
       {
         path: '*',
         component: AppComponent
+      },
+      {
+        path: 'reader',
+        component: ReaderComponent
+      },
+      {
+        path: 'games',
+        component: GamesComponent
+      },
+      {
+        path: 'shopping',
+        component: ShoppingComponent
       },
       {
         path: 'app/:appName/:route',
