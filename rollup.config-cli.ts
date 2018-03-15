cbffd7033dd5bca84fb8bf06326ae57c3e5621f1
Phodal HUANG <h@phodal.com>
2018-03-15 21:59:04 +0800

    [T] update cli config

---
+++ rollup.config-cli.ts
@@ -0,0 +1,34 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript2';
+
+const pkg = require('./package.json')
+
+const libraryName = 'cli'
+
+export default {
+  input: `src/${libraryName}.ts`,
+  output: [
+    { file: pkg.bin, format: 'es' },
+  ],
+  sourcemap: true,
+  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+  external: [],
+  watch: {
+    include: 'src/**',
+  },
+  plugins: [
+    // Compile TypeScript files
+    typescript({ useTsconfigDeclarationDir: true }),
+    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+    commonjs(),
+    // Allow node_modules resolution, so you can use 'external' to control
+    // which external modules to include in the bundle
+    // https://github.com/rollup/rollup-plugin-node-resolve#usage
+    resolve(),
+
+    // Resolve source maps to the original source
+    sourceMaps(),
+  ],
+}
