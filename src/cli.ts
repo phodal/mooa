5b85dc2574a0572ab4bf4adc203166a07147ca6a
Phodal HUANG <h@phodal.com>
2018-03-15 22:26:58 +0800

    [cli] add basci cli version

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,34 @@
-export function cli() {
-  return ''
+#!/usr/bin/env node
+
+const program = require('commander')
+
+program
+  .version('0.1.0')
+  .option('-c, --create [type]', 'Create [type] host || app')
+  .option('-u, --update [host]', 'Update [type] host || app')
+  .option('-g, --generate', 'Generate Mooa App Config')
+  .parse(process.argv)
+
+if (program.create) {
+  if (program.create === 'host') {
+    console.log('create host')
+  } else if (program.create === 'app') {
+    console.log('create app')
+  }
+}
+
+if (program.update) {
+  if (program.update === 'host') {
+    console.log('update host')
+  } else if (program.update === 'app') {
+    console.log('update app')
+  }
+}
+
+if (program.generate) {
+  console.log('generate')
+}
+
+if (!process.argv.slice(2).length || !process.argv.length) {
+  program.outputHelp()
 }
