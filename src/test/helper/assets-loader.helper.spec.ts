7aff929b43734a0999d4da393f82ff877c221356
Phodal HUANG <h@phodal.com>
2018-03-21 08:10:07 +0800

    [T] add test for assets loader

---
+++ src/test/helper/assets-loader.helper.spec.ts
@@ -0,0 +1,24 @@
+const assetsLoaderHelper = require('../../helper/assets-loader.helper').default
+
+test('should get hash code', () => {
+  let hash = assetsLoaderHelper.hashCode('hello, world')
+  expect(hash).toBe('543394210')
+})
+
+test('should be able create script tag', () => {
+  let scriptTag = assetsLoaderHelper.createScriptTag('/test.js')
+  let tmp = document.createElement('div')
+  tmp.appendChild(scriptTag)
+  expect(tmp.innerHTML).toBe(
+    '<script type="text/javascript" src="/test.js" charset="UTF-8" id="1494346482"></script>'
+  )
+})
+
+test('should be able create style tag', () => {
+  let scriptTag = assetsLoaderHelper.createLinkTag('/style.css')
+  let tmp = document.createElement('div')
+  tmp.appendChild(scriptTag)
+  expect(tmp.innerHTML).toBe(
+    '<link rel="stylesheet" href="/style.css" type="text/css" charset="UTF-8" id="2145530989">'
+  )
+})
