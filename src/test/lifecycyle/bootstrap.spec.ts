2f99a9455a51a655474429f7738ffd9bcbc59b07
Phodal HUANG <h@phodal.com>
2018-03-23 08:08:44 +0800

    [test] add bootstrap test

---
+++ src/test/lifecycyle/bootstrap.spec.ts
@@ -0,0 +1,40 @@
+import { StatusEnum } from '../../model/constants'
+import { toBootstrapPromise } from '../../lifecycles/bootstrap'
+import { ensureValidAppTimeouts } from '../../helper/timeouts'
+import { toLoadPromise } from '../../lifecycles/load'
+
+test('bootstrap mooa success', () => {
+  toBootstrapPromise({
+    name: 'mooa',
+    status: StatusEnum.NOT_BOOTSTRAPPED,
+    bootstrap: () => {
+      return new Promise((resolve, reject) => {
+        resolve()
+      })
+    },
+    timeouts: ensureValidAppTimeouts(3000)
+  }).then(app => {
+    expect(app.status).toBe(StatusEnum.NOT_MOUNTED)
+  })
+})
+
+test('load mooa success', () => {
+  toLoadPromise({
+    name: 'mooa',
+    status: StatusEnum.NOT_LOADED,
+    bootstrap: () => {
+      return new Promise((resolve, reject) => {
+        resolve()
+      })
+    },
+    timeouts: ensureValidAppTimeouts(3000)
+  }).then(app => {
+    expect(app.status).toBe(StatusEnum.NOT_BOOTSTRAPPED)
+    expect(typeof app.bootstrap).toBe('function')
+    expect(typeof app.load).toBe('function')
+    expect(typeof app.mount).toBe('function')
+    expect(typeof app.unload).toBe('function')
+    expect(typeof app.unmount).toBe('function')
+    expect(typeof app.timeouts).toBe('function')
+  })
+})
