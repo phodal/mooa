b8380f55c77f9654124de32a3f77ca267037cf22
Phodal HUANG <h@phodal.com>
2018-03-26 13:06:23 +0800

    [mooa] add more test for mooa

--- test/mooa.spec.ts
+++ test/mooa.spec.ts
@@ -52,3 +52,37 @@ test('new mooa by register', () => {
   expect((global as any).apps[0].name).toEqual('help')
   expect(typeof (global as any).mooa.instance).toEqual('object')
 })
+
+test('new mooa by with options', () => {
+  ;(global as any).apps = []
+  const mooa = new Mooa({
+    mode: 'iframe',
+    debug: false,
+    parentElement: 'app-home',
+    urlPrefix: 'app',
+    switchMode: 'coexist',
+    preload: true,
+    includeZone: true
+  })
+
+  let appConfig = {
+    name: 'help',
+    selector: 'app-help',
+    baseScriptUrl: '/assets/help',
+    styles: ['styles.bundle.css'],
+    prefix: 'help',
+    scripts: ['inline.bundle.js', 'polyfills.bundle.js', 'main.bundle.js'],
+    mode: 'iframe'
+  }
+
+  mooa.registerApplication('help', appConfig, mooaRouter.matchRoute('help'))
+  mooa.start()
+
+  let currentApp = (global as any).apps[(global as any).apps.length - 1]
+
+  console.log(currentApp)
+  expect(currentApp.name).toEqual('help')
+  expect(currentApp.mode).toEqual('iframe')
+  expect(currentApp.switchMode).toEqual('coexist')
+  expect(typeof (global as any).mooa.instance).toEqual('object')
+})
