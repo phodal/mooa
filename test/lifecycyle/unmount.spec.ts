ce5696da27a7de39203a741f5eaf298fb39077e4
Phodal HUANG <h@phodal.com>
2018-03-28 08:01:47 +0800

    [unmount] add simple test

--- test/lifecycyle/unmount.spec.ts
+++ test/lifecycyle/unmount.spec.ts
@@ -1,3 +1,12 @@
+import { addAppToUnload } from '../../src/lifecycles/unload'
+import { StatusEnum } from '../../src/model/constants'
+import { ensureValidAppTimeouts } from '../../src/helper/timeouts'
+import {
+  getAppNames,
+  toUnmountPromise,
+  unloadApplication
+} from '../../src/lifecycles/unmount'
+
 const jsdom = require('jsdom')
 const { JSDOM } = jsdom
 const { window, document } = new JSDOM()
@@ -5,10 +14,6 @@ const { window, document } = new JSDOM()
 const globalAny: any = global
 globalAny.window = window
 globalAny.document = document
-
-import { StatusEnum } from '../../src/model/constants'
-import { ensureValidAppTimeouts } from '../../src/helper/timeouts'
-import { toUnmountPromise } from '../../src/lifecycles/unmount'
 
 test('unmount mooa success', () => {
   let mockApp = {
@@ -30,8 +35,44 @@ test('unmount mooa success', () => {
     },
     timeouts: ensureValidAppTimeouts(3000)
   }
+  let appNames = getAppNames()
+  expect(appNames).toEqual(['help'])
   toUnmountPromise(app).then(app => {
     expect(unmountPromise).toBe(true)
     expect(app.status).toBe(StatusEnum.NOT_MOUNTED)
+  })
+})
+
+test('unloadApplication', () => {
+  let unmountPromise = false
+
+  let mockApp = {
+    name: 'help',
+    status: StatusEnum.MOUNTED,
+    unmount: (app: any) => {
+      unmountPromise = true
+      return new Promise((resolve, reject) => {
+        resolve()
+      })
+    },
+    unload: () => {
+      return new Promise((resolve, reject) => {
+        resolve()
+      })
+    },
+    timeouts: ensureValidAppTimeouts(3000)
+  }
+  globalAny.window.apps = [mockApp]
+  let app = {
+    name: 'help',
+    status: StatusEnum.MOUNTED
+  }
+  const resultPromise = new Promise((resolve, reject) => {
+    addAppToUnload(app, () => resultPromise, resolve, reject)
+  })
+  unloadApplication('help')
+
+  resultPromise.then(() => {
+    expect(unmountPromise).toBe(true)
   })
 })
